72e64a0902979dd80d444a37f89cbad3848f08c0
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2021-03-06 06:39:39 +0200

    added terser for minification and unglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,27 @@
 import babel from "@rollup/plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 const dist = "dist";
+const bundle = "bundle";
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: "src/index.js",
   external: ["react"],
   output: [
     {
-      file: `${dist}/bundle.cjs.js`,
+      file: `${dist}/${bundle}.cjs.js`,
       format: "cjs",
     },
     {
-      file: `${dist}/bundle.esm.js`,
+      file: `${dist}/${bundle}.esm.js`,
       format: "esm",
     },
     {
       name: "StarterKidLib",
-      file: `${dist}/bundle.umd.js`,
+      file: `${dist}/${bundle}.umd.js`,
       globals: {
         react: "React",
       },
@@ -29,5 +33,6 @@ export default {
     babel({
       exclude: "node_modules/**",
     }),
+    production && terser(),
   ],
 };
